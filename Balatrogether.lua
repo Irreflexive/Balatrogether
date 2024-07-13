cf54374d91fd23de20f50d4ae4f1a045b2f8668c
Irreflexive <33270232+Irreflexive@users.noreply.github.com>
2024-07-13 17:03:35 -0400

    initial commit

---
+++ Balatrogether.lua
@@ -0,0 +1,16 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Balatrogether
+--- MOD_ID: Balatrogether
+--- MOD_AUTHOR: [Irreflexive]
+--- MOD_DESCRIPTION: Play co-op Balatro with your friends!
+--- BADGE_COLOUR: 00FF64
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+function SMODS.INIT.Balatrogeter()
+  sendDebugMessage("Launching Balatrogeter!")
+end
+
+----------------------------------------------
+------------MOD CODE END----------------------
