08a4ba9f16b082202697b8e87a340e6c69cddf58
Irreflexive <33270232+Irreflexive@users.noreply.github.com>
2024-07-18 16:48:41 -0400

    network pack

---
+++ items/network_pack.lua
@@ -0,0 +1,23 @@
+local loc_def = {
+  name = "Network Pack",
+  text = {
+      "Choose {C:attention}#1#{} of up to ",
+      "{C:attention}#2# {C:attention}Playing{} cards or",
+      "{C:attention}Jokers{} from {C:attention}opponents{}",
+  }
+}
+
+SMODS.Booster{
+  name = "Network Pack",
+  key = "network",
+  pos = {x = 1, y = 1},
+  discovered = false,
+  loc_txt = loc_def,
+  weight = 1,
+  cost = 6,
+  config = {extra = 3, choose = 1},
+  atlas = "Balatrogether_cards",
+  in_pool = function(self)
+    return G.FUNCS.is_versus_game()
+  end,
+}
