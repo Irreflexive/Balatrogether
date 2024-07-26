b010f09a17aea5f193fee213d382be3006a87a32
Irreflexive <33270232+Irreflexive@users.noreply.github.com>
2024-07-26 09:57:53 -0400

    prevent parse failure when receiving multiple json objects

--- tcp_thread.lua
+++ tcp_thread.lua
@@ -17,10 +17,38 @@ while true do
   end
   local s, status, partial = tcp:receive()
   if status ~= "timeout" then
-    receive_channel:push({
-      s = s,
-      status = status,
-      partial = partial,
-    })
+    if s then
+      local messages = {}
+      local first = nil
+      local num_open = 0
+      for i = 1, #s do
+        local char = s:sub(i, i)
+        if char == "{" or char == "[" then
+          if not first then first = i end
+          num_open = num_open + 1
+        end
+
+        if first and (char == "}" or char == "]") then
+          num_open = num_open - 1
+          if num_open == 0 then
+            table.insert(messages, s:sub(first, i))
+            first = nil
+          end
+        end
+      end
+      for _,msg in ipairs(messages) do 
+        receive_channel:push({
+          s = msg,
+          status = status,
+          partial = partial,
+        })
+      end
+    else
+      receive_channel:push({
+        s = s,
+        status = status,
+        partial = partial,
+      })
+    end
   end
 end
