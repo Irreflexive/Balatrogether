7c4f143d11bb1deda630f8ab594ce20aff078394
Irreflexive <33270232+Irreflexive@users.noreply.github.com>
2024-07-30 13:43:17 -0400

    fix buy when full bug

--- actions/card_buttons.lua
+++ actions/card_buttons.lua
@@ -25,6 +25,12 @@ G.FUNCS.buy_from_shop = function(e)
   if G.FUNCS.is_coop_game() then
     local areaType = getCardAreaType(card.area, {G.shop_jokers})
     if not areaType then return end
+    if e.config.id ~= 'buy_and_use' then
+      if not G.FUNCS.check_for_buy_space(card) then
+        e.disable_button = nil
+        return
+      end
+    end
     G.FUNCS.tcp_send({ cmd = e.config.id == 'buy_and_use' and "BUY_AND_USE" or "BUY", index = card.Multiplayer_ID, type = areaType })
   else
     G.SINGLEPLAYER_FUNCS.buy_card(e)
@@ -52,7 +58,73 @@ G.FUNCS.tcp_listen("BUY", function(data)
     if not card then return end
     local e = findDescendantOfElementByConfig(card, "func", "can_buy")
     if not e then return end
-    G.SINGLEPLAYER_FUNCS.buy_card(e)
+    -- Here we copy and paste from buy_from_shop directly because there is a bug after buying when jokers are full
+    -- TODO: Find a way to do this without copying the source
+    G.E_MANAGER:add_event(Event({
+      trigger = 'after',
+      delay = 0.1,
+      func = function()
+        card.area:remove_card(card)
+        card:add_to_deck()
+        if card.children.price then card.children.price:remove() end
+        card.children.price = nil
+        if card.children.buy_button then card.children.buy_button:remove() end
+        card.children.buy_button = nil
+        remove_nils(card.children)
+        if card.ability.set == 'Default' or card.ability.set == 'Enhanced' then
+          inc_career_stat('c_playing_cards_bought', 1)
+          G.playing_card = (G.playing_card and G.playing_card + 1) or 1
+          G.deck:emplace(card)
+          card.playing_card = G.playing_card
+          playing_card_joker_effects({card})
+          table.insert(G.playing_cards, card)
+        elseif e.config.id ~= 'buy_and_use' then
+          if card.ability.consumeable then
+            G.consumeables:emplace(card)
+          else
+            G.jokers:emplace(card)
+          end
+          G.E_MANAGER:add_event(Event({func = function() card:calculate_joker({buying_card = true, card = card}) return true end}))
+        end
+        --Tallies for unlocks
+        G.GAME.round_scores.cards_purchased.amt = G.GAME.round_scores.cards_purchased.amt + 1
+        if card.ability.consumeable then
+          if card.config.center.set == 'Planet' then
+            inc_career_stat('c_planets_bought', 1)
+          elseif card.config.center.set == 'Tarot' then
+            inc_career_stat('c_tarots_bought', 1)
+          end
+        elseif card.ability.set == 'Joker' then
+          G.GAME.current_round.jokers_purchased = G.GAME.current_round.jokers_purchased + 1
+        end
+
+        for i = 1, #G.jokers.cards do
+          G.jokers.cards[i]:calculate_joker({buying_card = true, card = card})
+        end
+
+        if G.GAME.modifiers.inflation then 
+          G.GAME.inflation = G.GAME.inflation + 1
+          G.E_MANAGER:add_event(Event({func = function()
+            for k, v in pairs(G.I.CARD) do
+                if v.set_cost then v:set_cost() end
+            end
+            return true end }))
+        end
+
+        play_sound('card1')
+        inc_career_stat('c_shop_dollars_spent', card.cost)
+        if card.cost ~= 0 then
+          ease_dollars(-card.cost)
+        end
+        G.CONTROLLER:save_cardarea_focus('jokers')
+        G.CONTROLLER:recall_cardarea_focus('jokers')
+
+        if e.config.id == 'buy_and_use' then 
+          G.SINGLEPLAYER_FUNCS.use_card(e, true)
+        end
+        return true
+      end
+    }))
   else
     local area = G[data.type]
     local areaType = getCardAreaType(area, {G.shop_booster, G.shop_vouchers, G.pack_cards})
